a942f7111f4fed55b04edefac994f9e63b6e1719
nimuthusanira <nimuthuyt@gmail.com>
2026-08-31 20:07:52 +0530

    Resolve merge conflict in user.js

--- server/models/user.js
+++ server/models/user.js
@@ -2,24 +2,6 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-<<<<<<< HEAD
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-});
-
-// Hash password before saving to database
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
-// Method to compare entered login password with stored hashed password
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-=======
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
@@ -34,7 +16,6 @@ userSchema.pre('save', async function (next) {
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
->>>>>>> nimuthum
 };
 
 module.exports = mongoose.model('User', userSchema);
