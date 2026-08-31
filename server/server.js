06414e483650836c83c0802ebb273c68ed9140e4
nimuthusanira <nimuthuyt@gmail.com>
2026-08-31 15:56:47 +0530

    Complete Part 2: User Authentication & Security

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 connectDB();
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('API is running...'));
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
